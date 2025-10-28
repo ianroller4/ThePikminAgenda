32e13861a25393eb428930ad93e7fd0fbc32fb86
Jangho Son <json66@rrc.ca>
2025-10-27 20:38:27 -0500

    Update Enemy.cs
    Update Enemy.cs : add Idle statement

--- Assets/Scripts/Movement/Enemy.cs
+++ Assets/Scripts/Movement/Enemy.cs
@@ -16,12 +16,25 @@ public class Enemy : MonoBehaviour
 
     private EnemyState currentState;
 
+    // --- Component References ---
     private Rigidbody2D rigid;
+
+    // --- Variables ---
+    private Vector3 startPos;
+    private Vector3 targetPos;
+    private float speed;
+
+    // --- Timers ---
+    private float idleTimer = 0f;
 
 
     void Start()
     {
         rigid = GetComponent<Rigidbody2D>();
+
+        startPos = transform.position;
+
+        currentState = EnemyState.Idle;
     }
 
     // Update is called once per frame
@@ -56,8 +69,28 @@ public class Enemy : MonoBehaviour
     }
 
     private void Idle()
-    {
+    { 
+        float dist = Vector2.Distance(transform.position, startPos);
 
+        // limit the monster's movement range to within 50 units
+        if (dist < 50 && idleTimer <= 0f)
+        {
+
+            targetPos = transform.position;
+
+            Vector2 randomDir = Random.insideUnitCircle.normalized;
+
+            targetPos = transform.position + (Vector3)(randomDir * 0.5f);
+
+            idleTimer = Random.Range(1f, 2f);
+        }
+        else if(dist >= 50)
+        {
+            targetPos = startPos;
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, targetPos, Time.deltaTime * 1f);
+        idleTimer -= Time.deltaTime;
     }
 
     private void Sleep()
