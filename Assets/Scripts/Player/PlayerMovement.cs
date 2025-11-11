7ef87e4988c4b2debac6210597b362da672aebc0
ianroller4 <ianroller4@gmail.com>
2025-11-11 13:04:39 -0600

    Animations
    Integrated animations for player and most animations for SLG (do not have death implemented need to discuss).

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -12,10 +12,15 @@ public class PlayerMovement : MonoBehaviour
     public float speed = 5f;
     private Vector2 input;
 
+    // --- Animator ---
+    private Animator animator;
+    private Vector2 lastDir = Vector2.zero;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -30,6 +35,19 @@ public class PlayerMovement : MonoBehaviour
         input.x = Input.GetAxisRaw("Horizontal");
         input.y = Input.GetAxisRaw("Vertical");
         input = input.normalized;
+        if (input != Vector2.zero)
+        {
+            animator.SetBool("idle", false);
+            lastDir = input;
+            animator.SetFloat("x", input.x);
+            animator.SetFloat("y", input.y);
+        }
+        else
+        {
+            animator.SetBool("idle", true);
+            animator.SetFloat("x", lastDir.x);
+            animator.SetFloat("y", lastDir.y);
+        }
     }
 
     private void Move()
