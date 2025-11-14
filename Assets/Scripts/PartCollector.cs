e5c9dd29bbcdaf028c815bf714d72f7fbe085053
ianroller4 <ianroller4@gmail.com>
2025-11-14 10:09:33 -0600

    End Condition

---
+++ Assets/Scripts/PartCollector.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SceneLoader))]
+[RequireComponent(typeof(Collider2D))]
+public class PartCollector : MonoBehaviour
+{
+    public int partsToWin = 3;
+    private int partCount = 0;
+    private SceneLoader sceneLoader;
+
+    private void Start()
+    {
+        sceneLoader = GetComponent<SceneLoader>();
+    }
+
+    private void Update()
+    {
+        if (partCount >= partsToWin)
+        {
+            sceneLoader.LoadNextScene(2);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        GameObject go = collision.gameObject;
+        if (go != null)
+        {
+            if (go.GetComponent<CarryableObject>() != null)
+            {
+                partCount++;
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        GameObject go = collision.gameObject;
+        if (go != null)
+        {
+            if (go.GetComponent<CarryableObject>() != null)
+            {
+                partCount--;
+                if (partCount < 0)
+                {
+                    partCount = 0;
+                }
+            }
+        }
+    }
+}
